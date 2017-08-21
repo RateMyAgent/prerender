b6d578dff071b1e4995f0e6b5efd1344163f8389
Greg Austin <greg.austin@ratemyagent.com>
2017-08-21 21:48:18 +1000

    softy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    softIterations: process.env.PRERENDER_NUM_ITERATIONS
 });
 
 server.use(prerender.sendPrerenderHeader());
